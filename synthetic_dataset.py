310b07b1143318d49f6015a06ffd020dde0c6436
Katherine Lambert <k.lambertcanada@gmail.com>
2025-06-24 14:54:52 -0400

    implemented the loop and fixed errors

--- synthetic_dataset.py
+++ synthetic_dataset.py
@@ -1,12 +1,18 @@
-import psutil
 import torch
 import datasets
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM
 import config
-import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
 
 dataset = datasets.load_from_disk(config.dataset_path)
+dataloader = DataLoader(dataset["train"].select(range(100)), batch_size=2)
+
+tokenizer = AutoTokenizer.from_pretrained(config.teacher_model_name)
+tokenizer.padding_side = "left"
+if tokenizer.pad_token is None:
+    print("tokenizer.pad_token does not exist")
 
 teacher = AutoModelForCausalLM.from_pretrained(
     config.teacher_model_name,
@@ -23,14 +29,25 @@ del student
 teacher.eval()
 ds = {"input_ids": [], "attention_mask": [], "labels": []}
 
-for batch in tqdm(dataset["train"].select(range(10))):
+n = 0
+
+print("\n=== GENERATING TEACHER LOGITS ===")
+
+for batch in tqdm(dataloader):
+    n += 1
+
+    if n > 10:
+        break
+
     input_ids = batch["input_ids"].to(config.teacher_device)
     attention_mask = batch["attention_mask"].to(config.teacher_device)
 
-    ds["input_ids"].append(input_ids)
-    ds["attention_mask"].append(attention_mask)
+    # print(f"batch size: {batch}")
+    # print(f"batch size: {batch}")
+    # print(f"Input_ids.shape: {input_ids.shape}")  # bathc_size, scores: FloatTensor
 
-    print(f"generating")
+    ds["input_ids"].append(input_ids.squeeze(0).cpu())
+    ds["attention_mask"].append(attention_mask.squeeze(0).cpu())
 
     with torch.no_grad():
         generation_output = teacher.generate(
@@ -38,19 +55,27 @@ for batch in tqdm(dataset["train"].select(range(10))):
             attention_mask=attention_mask,
             temperature=0.5,
             max_new_tokens=1024,
+            return_dict_in_generate=True,
+            output_scores=True,
         )
 
         generated_sequences = generation_output.sequences
+        print(f"generated sequences: {type(generated_sequences)}")
+
         gen_only = generated_sequences[:, input_ids.shape[1] :]
-        pad = torch.full((gen_only.size(0), input_ids.size(1)), fill_value=-100, dtype=gen_only.dtype)
+        print(f"gen_only: {type(gen_only)}")
+        print(f"gen_only: {gen_only.device}")
+
+        pad = torch.full((gen_only.size(0), input_ids.size(1)), fill_value=-100, dtype=gen_only.dtype, device=gen_only.device)
+        print(f"pad: {type(pad)}")
+        print(f"pad: {pad.device}")
         labels = torch.cat([pad, gen_only], dim=1)
 
-        ds["labels"].append(labels)
+        ds["labels"].append(labels.squeeze(0).cpu())
 
         del generation_output, generated_sequences, gen_only, pad, input_ids, attention_mask
         torch.cuda.empty_cache()
 
-print("\n=== GENERATING TEACHER LOGITS ===")
 dset = datasets.Dataset.from_dict(ds)  # turn into huggingface dict
 dset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 dset.save_to_disk(config.synthetic_dataset_path)
