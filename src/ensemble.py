d583c34037b0d2dd1f550c4c56ed1052a96a52a8
Katherine Lambert <k.lambertcanada@gmail.com>
2025-08-23 16:09:42 -0400

    Document device placement strategy for ensemble models

--- src/ensemble.py
+++ src/ensemble.py
@@ -31,8 +31,6 @@ class ModelEnsemble(PreTrainedModel, GenerationMixin):
         modules = []
         for path in model_paths:
             model = AutoModelForCausalLM.from_pretrained(model_type, torch_dtype=self.torch_dtype)
-            # TODO: are these models placed on device in the main or train_single_round? Is it better to keep them on the cpu or distribute it to the available gpus?
-            # Or are the gpus better used for the sharded student model?
             state_dict = torch.load(path / "model_state_dict.pt", weights_only=True, map_location='cpu')
             model.load_state_dict(state_dict)
             model.eval()
